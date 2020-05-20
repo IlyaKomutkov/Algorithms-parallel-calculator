b3e4e5cc2b9549b5537f672587099d9ae5010f0c
Tim Vilkov <stocer05@yandex.ru>
2020-05-20 19:16:48 +0300

    Some upgrades
    Fixed and updated Controller and added ThreadTesting

--- main/Controller.hpp
+++ main/Controller.hpp
@@ -4,64 +4,88 @@
 #include <condition_variable>
 #include <algorithm>
 #include<map>
-using namespace std;
 
 template <size_t N, class T>
-class FixedThreadPool
+class Controller
 {
-	mutex qm, qf;
-	condition_variable done;
-	deque<T> que;
+	/*This class is using for parallel calculations*/
+	std::mutex mutex1, mutex2;
+	std::condition_variable done;
+	std::deque<T> que;
+	std::deque<std::string>PATHS;
 	bool Finished[N];
-	void reset()
+	void reset()//Initializing bool variables
 	{
-		for (auto& i : Finished)
+		for (bool& i : Finished)
 		{
 			i = false;
 		}
 	}
-	void thread_task(size_t id)
+	void thread_task(size_t id)//Make calculations in thread that got as parametr of function
 	{
 		while (true)
 		{
-			unique_lock<mutex> lock(qm);
+			std::unique_lock<std::mutex> lock(mutex1);
 			if (que.empty())
 			{
-				unique_lock<mutex> lock(qf);
+				std::unique_lock<std::mutex> lock(mutex2);
 				Finished[id] = true;
-				done.notify_all();
+				done.notify_all();//Unblocks all threads currently waiting for this condition
 				return;
 			}
-			auto job = move(que.front());
+			int i = 0;
+			auto job = std::move(que.front());//Elements are taken in turn(one by one) till deque is not empty 
 			que.pop_front();
 			lock.unlock();
-			//	std::string fileName = "test" + std::to_string(i++) + ".csv"
-			int i = 0;
-			std::string a = "pisia " + to_string(i++) + ".csv";
-			job->start(a);
+			const char firstSymbol = PATHS[0].front();//For message in concole
+			PATHS.pop_front();
+			std::string algName, resultPATH;
+			if (firstSymbol == 'R') 
+			{
+				algName = "RK Integrator Algorithm ";
+				resultPATH = "RK/results/TwoBodyOdeSolution.csv";
+			}
+			else
+			{
+				algName = "KMeans Algorithm";
+				resultPATH = "KMEans/results/ThreeBodyOdeSolution.csv";
+			}
+			job->start(resultPATH);//Method "start" starts calculations of i-th element of deque
+			std::cout << algName<<std::endl 
+			<<"Thread id: "<< std::this_thread::get_id() << std::endl
+			<<resultPATH<<std::endl<<std::endl;
+			i++;
+
 		}
+
 	}
 public:
-	void push(const T job)
+	void push(const T job, const std::string path)//Adding object in queue
 	{
-		unique_lock<mutex> lock(qm);
-		que.emplace_back(move(job));
+
+		std::unique_lock<std::mutex> lock(mutex1);
+		que.emplace_back(std::move(job));
+		PATHS.emplace_back(path);
 	}
-	void start()
+	void start()//Implementation of the algorithm
 	{
 		reset();
 		for (size_t i = 0; i < N; i++)
 		{
-			thread worker([this, i]() {thread_task(i); });
+			std::thread worker([this, i]() {
+				thread_task(i); 
+			});
 			worker.detach();
 		}
 	}
-	void wait_finished()
+	void wait_finished()//Controlling the end of process
 	{
-		unique_lock<mutex> lock(qf);
+		std::unique_lock<std::mutex> lock(mutex2);
 		done.wait(lock, [this]
 		{
-			return all_of(Finished, Finished + N - 1, [](bool e) {return e; });
+			return std::all_of(Finished, Finished + N - 1, [](bool e) {return e; });
 		});
 	}
 };
+
+
