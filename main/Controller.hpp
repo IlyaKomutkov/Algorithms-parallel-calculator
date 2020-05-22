58e37123737f2965e8aeb01d237b7598f9a6a3a1
Anatoly <krest.66@mail.ru>
2020-05-22 22:35:51 +0300

    Revert "Some upgrades"
    This reverts commit b3e4e5cc2b9549b5537f672587099d9ae5010f0c.

--- main/Controller.hpp
+++ main/Controller.hpp
@@ -4,88 +4,64 @@
 #include <condition_variable>
 #include <algorithm>
 #include<map>
+using namespace std;
 
 template <size_t N, class T>
-class Controller
+class FixedThreadPool
 {
-	/*This class is using for parallel calculations*/
-	std::mutex mutex1, mutex2;
-	std::condition_variable done;
-	std::deque<T> que;
-	std::deque<std::string>PATHS;
+	mutex qm, qf;
+	condition_variable done;
+	deque<T> que;
 	bool Finished[N];
-	void reset()//Initializing bool variables
+	void reset()
 	{
-		for (bool& i : Finished)
+		for (auto& i : Finished)
 		{
 			i = false;
 		}
 	}
-	void thread_task(size_t id)//Make calculations in thread that got as parametr of function
+	void thread_task(size_t id)
 	{
 		while (true)
 		{
-			std::unique_lock<std::mutex> lock(mutex1);
+			unique_lock<mutex> lock(qm);
 			if (que.empty())
 			{
-				std::unique_lock<std::mutex> lock(mutex2);
+				unique_lock<mutex> lock(qf);
 				Finished[id] = true;
-				done.notify_all();//Unblocks all threads currently waiting for this condition
+				done.notify_all();
 				return;
 			}
-			int i = 0;
-			auto job = std::move(que.front());//Elements are taken in turn(one by one) till deque is not empty 
+			auto job = move(que.front());
 			que.pop_front();
 			lock.unlock();
-			const char firstSymbol = PATHS[0].front();//For message in concole
-			PATHS.pop_front();
-			std::string algName, resultPATH;
-			if (firstSymbol == 'R') 
-			{
-				algName = "RK Integrator Algorithm ";
-				resultPATH = "RK/results/TwoBodyOdeSolution.csv";
-			}
-			else
-			{
-				algName = "KMeans Algorithm";
-				resultPATH = "KMEans/results/ThreeBodyOdeSolution.csv";
-			}
-			job->start(resultPATH);//Method "start" starts calculations of i-th element of deque
-			std::cout << algName<<std::endl 
-			<<"Thread id: "<< std::this_thread::get_id() << std::endl
-			<<resultPATH<<std::endl<<std::endl;
-			i++;
-
+			//	std::string fileName = "test" + std::to_string(i++) + ".csv"
+			int i = 0;
+			std::string a = "pisia " + to_string(i++) + ".csv";
+			job->start(a);
 		}
-
 	}
 public:
-	void push(const T job, const std::string path)//Adding object in queue
+	void push(const T job)
 	{
-
-		std::unique_lock<std::mutex> lock(mutex1);
-		que.emplace_back(std::move(job));
-		PATHS.emplace_back(path);
+		unique_lock<mutex> lock(qm);
+		que.emplace_back(move(job));
 	}
-	void start()//Implementation of the algorithm
+	void start()
 	{
 		reset();
 		for (size_t i = 0; i < N; i++)
 		{
-			std::thread worker([this, i]() {
-				thread_task(i); 
-			});
+			thread worker([this, i]() {thread_task(i); });
 			worker.detach();
 		}
 	}
-	void wait_finished()//Controlling the end of process
+	void wait_finished()
 	{
-		std::unique_lock<std::mutex> lock(mutex2);
+		unique_lock<mutex> lock(qf);
 		done.wait(lock, [this]
 		{
-			return std::all_of(Finished, Finished + N - 1, [](bool e) {return e; });
+			return all_of(Finished, Finished + N - 1, [](bool e) {return e; });
 		});
 	}
 };
-
-
