5d40f569bf88732e50a0f7ced7f38a04cc8343ec
Anatoly <krest.66@mail.ru>
2020-05-28 19:55:03 +0300

    Revert "refactoring"
    This reverts commit 4aef116126f38f615a14c06cc1d7de019adecfdf.

--- main/Controller.hpp
+++ main/Controller.hpp
@@ -63,43 +63,46 @@ class ThreadController {
 	}
 
 public:
-
-	//Add algorithm and path in queue
+	//Add object in queue
 	ThreadController& push(T algorithm, const std::string path) {
 		algVector.push_back(algorithm);
 		PATHS.push_back(path);
 		return *this;
 	}
 
-	//Add algorithm and path in queue
-	ThreadController& push(std::pair<T, std::string> algorithm) {
+	//Add object in queue
+	ThreadController& push(std::pair<T,std::string>algorithm) {
 		algVector.push_back(algorithm.first);
 		PATHS.push_back(algorithm.second);
 		return *this;
 	}
-
-	//Add object in queue
-	ThreadController& push(std::vector<std::pair<T, std::string>> data) {
-		for(auto i : data) {
+		//Add object in queue
+	ThreadController& push(std::vector<std::pair<T,std::string>>data) {
+		for(auto i: data)
+		{
 			algVector.push_back(i.first);
 			PATHS.push_back(i.second);
 		}
 		return *this;
 	}
 
-	// Add algorithms and paths from vectors in queue
-	ThreadController& push(std::vector<T>algorithms, std::vector<std::string> filenames) {
+			//Add object in queue
+	ThreadController& push(std::vector<T>algorithms,std::vector<std::string>filenames) {
 		for(auto i: algorithms)
+		{
 			algVector.push_back(i);
+		}
 
 		for(auto j: filenames)
+		{
 			PATHS.push_back(j);
-
+		}
 		return *this;
 	}
 
 	//Implementation of the algorithm
 	void start() {
+
 		for (bool& i : Finished)
 			i = false;
 
@@ -109,6 +112,7 @@ public:
 			});
 			worker.detach();
 		}
+
 		this->waitFinished();
 	}
 };
