0e5f859ddd66230dc415d962559af7a784c6f4a8
evo02 <evo02t@gmail.com>
2020-05-29 21:02:56 +0500

    KMetrics fixed

--- main/KMetrics.hpp
+++ main/KMetrics.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "linalg.hpp"
-
 
 class Metrics {
 public:
@@ -15,26 +13,14 @@ public:
 class ManhattanDist : public Metrics
 {
 public:
-	long double distance(std::valarray<long double> v1, std::valarray<long double> v2) override {
-		//std::cout << "ManhattanDist" << std::endl;
-		long double s = 0;
-		for (size_t i {0}; i < v1.size(); ++i)
-		s += abs(v1[i] - v2[i]);
-		return s;
-	}
+	long double distance(std::valarray<long double> v1, std::valarray<long double> v2) override;
 };
 
 
 //Chebyshev distance
 class ChebyshevDist : public Metrics {
 public:
-	long double distance(std::valarray<long double> v1, std::valarray<long double> v2) override {
-		//std::cout << "ChebyshevDist" << std::endl;
-		long double s = 0;
-		for (size_t i{ 0 }; i < v1.size(); ++i)
-			s = std::max(s, abs(v1[i] - v2[i]));
-		return s;
-	}
+	long double distance(std::valarray<long double> v1, std::valarray<long double> v2) override;
 };
 
 
@@ -43,23 +29,12 @@ class MinkowskiDist : public Metrics {
 	long double power_ = 1;
 public:
 	MinkowskiDist(long double pow) : power_{ pow } { }
-	long double distance(std::valarray<long double> v1, std::valarray<long double> v2) override {
-		//std::cout << "MinkowskiDist" << std::endl;
-		long double s = 0;
-		for (size_t i{ 0 }; i < v1.size(); ++i)
-		 	s += pow(pow(abs(v1[i] - v2[i]), power_), 1 / power_);
-		return s;
-	}
+	long double distance(std::valarray<long double> v1, std::valarray<long double> v2) override;
 };
 
 
 //Euclid distance
 class EuclidDist : public Metrics {
 public:
-	long double distance(std::valarray<long double> v1, std::valarray<long double> v2) override {
-		long double s = 0;
-		for (size_t i{ 0 }; i < v1.size(); ++i)
-			s += (v1[i] - v2[i]) * (v1[i] - v2[i]);
-		return sqrt(s);
-	}
+	long double distance(std::valarray<long double> v1, std::valarray<long double> v2) override;
 };
